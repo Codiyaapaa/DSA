38fb49fb1683acdd9807ef29f069b1adc24b8092
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2025-10-19 01:44:06 +0530

    Time: 301 ms (14.92%) | Memory: 151.6 MB (6.03%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -7,17 +7,20 @@ public:
         nums[0] = nums[0] - k;
         int prev = nums[0];
         st.insert(nums[0]);
+        // int cnt = 1;
         for(int i=1; i<n; i++){
             if( k == 0 ){
                 st.insert(nums[i]);
+                // cnt++;
                 continue;
             } 
             int x = max(prev+1,nums[i]-k);
             if(x > nums[i] + k) continue;
             prev = x;
             st.insert(prev);
+            // cnt++;
         }
-        for(auto it : st) cout<<it<<" ";
+        // for(auto it : st) cout<<it<<" ";
         return st.size();
     }
 };

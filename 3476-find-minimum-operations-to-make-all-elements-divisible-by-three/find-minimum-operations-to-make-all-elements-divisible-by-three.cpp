f7ab5b36c1035c4bda2ad1107e5d7089aa729697
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2025-11-22 09:09:17 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (10.78%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -3,7 +3,7 @@ public:
     int minimumOperations(vector<int>& nums) {
         int ans = 0;
         for(int i=0; i<nums.size(); i++){
-            cout<<3-(nums[i]%3)<<" "<<nums[i]<<endl;
+            // cout<<3-(nums[i]%3)<<" "<<nums[i]<<endl;
             if(nums[i]%3 != 0) ans += min(3-(nums[i]%3),nums[i]%3);
         }
         return ans;

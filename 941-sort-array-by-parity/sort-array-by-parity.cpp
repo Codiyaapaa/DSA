f469af56ed18543e478c6da036abff5a69c5e8d5
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2025-12-16 17:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (36.82%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -4,7 +4,16 @@ public:
         return a%2 < b%2;
     }
     vector<int> sortArrayByParity(vector<int>& nums) {
-        sort(nums.begin(),nums.end(),cmp);
+        // sort(nums.begin(),nums.end(),cmp);
+        // return nums;
+        int n = nums.size();
+        int even = 0;
+        int odd = n-1;
+        while(even < odd){
+            while(even<odd && nums[even]%2==0) even++;
+            while(odd>even && nums[odd]%2==1) odd--;
+            swap(nums[even],nums[odd]);
+        }
         return nums;
     }
 };

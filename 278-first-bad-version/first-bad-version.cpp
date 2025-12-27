a3b05e2cbf85b6740d65dadc4f5fd645d1287611
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2025-12-27 17:11:38 +0530

    Time: 3 ms (16.20%) | Memory: 7.8 MB (70.32%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,16 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int i=1;
+        int j=n;
+        while(i<=j){
+            long long mid = i + (j-i)/2;
+            if(isBadVersion(mid)) j = mid-1;
+            else i = mid + 1;
+        }
+        return i;
+    }
+};

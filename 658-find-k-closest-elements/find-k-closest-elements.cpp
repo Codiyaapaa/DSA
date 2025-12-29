a514dfed5ee7bbee068c96466c5cdcca0d9b5862
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2025-12-29 23:48:18 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (91.29%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -2,17 +2,30 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         // method 1 : using priority_queue
-        priority_queue<pair<int,int>> pq;
-        for(int i=0; i<arr.size(); i++){
-            pq.push({abs(arr[i]-x),arr[i]});
-            if(pq.size() > k) pq.pop();
+        // priority_queue<pair<int,int>> pq;
+        // for(int i=0; i<arr.size(); i++){
+        //     pq.push({abs(arr[i]-x),arr[i]});
+        //     if(pq.size() > k) pq.pop();
+        // }
+        // vector<int> ans;
+        // while(!pq.empty()){
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans;
+
+        //using two pointer
+        int i=0;
+        int j=arr.size()-1;
+        // cout<<i<<" "<<j<<endl;
+        while(j-i+1 > k){
+            if(abs(arr[i]-x) > abs(arr[j]-x)){
+                i++;
+            }
+            else j--;
         }
-        vector<int> ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+        // cout<<i<<" "<<j;
+        return vector<int>(arr.begin()+i,arr.begin()+j+1);
     }
 };

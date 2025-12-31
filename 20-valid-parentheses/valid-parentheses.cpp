8553ca5c978fb1f0e90f13ace88cdd826fb40d62
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2025-12-31 07:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.55%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto& it : s){
+            if(st.empty() && (it == ')' || it == '}' || it == ']')) return false;
+            else if(it == ')' && st.top() == '(' || it == '}' && st.top() == '{' || it == ']' && st.top() == '[') st.pop();
+            else st.push(it);
+        }
+        return st.empty();
+    }
+};

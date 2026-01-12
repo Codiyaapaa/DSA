b28148c8831745b47995c1612310306b3c0d2048
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2026-01-12 21:59:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (11.72%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n = s.size();
+        int maxi = 0;
+        stack<int> st;
+        st.push(-1);
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '(') st.push(i);
+            else{
+                st.pop();
+                if(st.empty()) st.push(i);
+                else maxi++;
+            }
+        }
+        cout<<n<<" "<<maxi;
+        return (n - maxi*2);
+    }
+};

454ffdc118eced375da544fbc6f2cf1d632bf056
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2026-02-06 19:35:16 +0530

    Time: 3 ms (89.92%) | Memory: 25.6 MB (71.39%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        for(int i=0; i<n; i++){
+            int j = (((i + nums[i])%n)+n)%n;
+            res[i] = nums[j];
+        }
+        // cout<<(-11%3);
+        return res;
+    }
+};

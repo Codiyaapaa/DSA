767354693e4531da164a364bbf1c70d690547269
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2026-03-21 01:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (13.32%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -17,6 +17,21 @@ public:
     }
 
     int maxDepth(TreeNode* root) {
-        return solve(root);
+        // return solve(root);
+        if(!root) return 0;
+        int ans = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            for(int i=0; i<n; i++){
+                auto it = q.front();
+                q.pop();
+                if(it->left) q.push(it->left);
+                if(it->right) q.push(it->right);
+            }
+            ans++;
+        }
+        return ans;
     }
 };

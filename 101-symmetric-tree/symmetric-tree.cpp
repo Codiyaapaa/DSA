b6faf684e7cd5108fbc043824d322941930580bb
Codiyaapaa <115916038+Codiyaapaa@users.noreply.github.com>
2026-03-20 22:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (98.19%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -14,12 +14,13 @@ public:
     bool solve(TreeNode* root1, TreeNode* root2){
         if(root1 == NULL || root2 == NULL) return root1==root2;
         if(root1->val != root2->val) return false;
-        return (solve(root1->left,root2->right) && solve(root1->right,root2->left));
+        bool left1 = solve(root1->left,root2->right);
+        bool right1 = solve(root1->right,root2->left); 
+        return left1 && right1;
     }
 
     bool isSymmetric(TreeNode* root) {
         if(!root) return true;
-
         return solve(root->left,root->right);
     }
 };
